52cd7cb10aab6e2b7525ab1bcec8adb472110ab5
IPilatova <irina.pilatova@gmail.com>
2021-08-25 11:26:34 +0300

    upd HomeWorkAppL3.java

--- HomeWorkAppL3.java
+++ HomeWorkAppL3.java
@@ -7,17 +7,17 @@ public class HomeWorkAppL3 {
 //1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
         int [] arrayChangeZeroOne = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         changeArrayElement(arrayChangeZeroOne);
-        readArray(arrayChangeZeroOne);
+        readArray("Задание 1 > ", arrayChangeZeroOne);
 
 //2. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
         int [] emptyIntArray = new int[100];
         fillEmptyArray(emptyIntArray);
-        readArray(emptyIntArray);
+        readArray("Задание 2 > ", emptyIntArray);
 
 //3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
         int [] arrayChangeLess6 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         changeNumLess6(arrayChangeLess6);
-        readArray(arrayChangeLess6);
+        readArray("Задание 3 > ", arrayChangeLess6);
 
 //4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей, если обе сложно). Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
         fillArrayDiagonals(5, 5);
@@ -31,8 +31,8 @@ public class HomeWorkAppL3 {
     }
 
 
-//  Метод для печати массива дл заданий 1-3
-    public static void readArray(int[] inputArray) {
+//  Вспомогательный метод. Печать  одномерного массива (для заданий 1-3)
+    public static void readArray(String msg, int[] inputArray) {
         for (int i = 0; i < inputArray.length; i++) {
             System.out.print(inputArray[i] + "\t");
         }
@@ -41,14 +41,15 @@ public class HomeWorkAppL3 {
 
 //1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
     public static void changeArrayElement(int[] inputArray) {
-        for (int i = 0; i < inputArray.length; i++) {
+        for (int i = 0; i < inputArray.length; i++)
             if (inputArray[i] == 1) {
                 inputArray[i] = 0;
             } else {
                 inputArray[i] = 1;
             }
-        }
+//        inputArray[i] = (inputArray[i] == 1) ? 0 : 1;
     }
+
 
 //2. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
     public static void fillEmptyArray(int[] inputArray) {
@@ -62,6 +63,7 @@ public class HomeWorkAppL3 {
         for (int i = 0; i < inputArray.length; i++) {
             if (inputArray[i] < 6) {
                 inputArray[i] = inputArray[i] * 2;
+//              inputArray[i] *= 2;
             }
         }
     }
